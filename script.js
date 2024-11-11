b86f4bf1d88012ae0b58827d3561963ccb3e8f2e
Ibrohim <Ibrohim>
2024-11-11 19:23:10 +0500

    js-4.2

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let name = +prompt("Ismingizni kiriting");
+
+let age = +prompt("Yoshingizni kiriting");
+
+let math1 = +prompt("Misolni Yeching: 6 + 3 = ?");
+
+let math2 = +prompt("Misolni Yeching: 20 - 5 = ?");
+
+let math3 = +prompt("Misolni Yeching: 15 * 2 = ?");
+
+let math4 = +prompt("Misolni Yeching: 8 / 4 = ?");
+
+let math5 = +prompt("Misolni Yeching 10 % 3 = ?");
+
+alert("Consolni oching");
+
+console.log("Sizning ismingiz ")+name;
+
+console.log("Sizning yoshingiz ")+age;
+
+console.log("1-Misol: 6 + 3 = 9. Sizning javobingiz ")+math1;
+
+console.log("2-Misol: 20 - 5 = 15. Sizning javobingiz ")+math2;
+
+console.log("3-Misol: 15 * 2 = 30. Sizning javobingiz ")+math3;
+
+console.log("4-Misol: 8 / 4 = 2. Sizning javobingiz ")+math4;
+
+console.log("5-Misol: 10 % 3 = 1. Sizning javobingiz ")+math5;
+
+
